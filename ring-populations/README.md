6e0123cf0ac57e9793a7ba46a3a9044cfe22a78c
alifeee <alifeeerenn@hotmail.com>
2023-06-01 23:29:44 +0100

    add ring population data n stuff

---
+++ ring-populations/README.md
@@ -0,0 +1,24 @@
+# Ring populations
+
+I often like to compare the populations of places. Mainly cities. However, I find that the google-results for this are not great.
+
+Thus, in this repository I'm looking at the population held within a concentric circle centred on a city, to see if it makes a good comparison.
+
+I expect there will be problems, like:
+
+- unfair representation of coastal cities (as circle goes into water)
+- difficulty in choosing size of circle:
+  - too small, and it may under-represent the urban area of the city
+  - too large, and it may include unwanted population
+  - too large, and it may end up mostly underwater for coastal cities
+
+## Population websites
+
+Usually if I want to find the population of a city, I just Google it. This may be why I end up with inconsistent results. Many websites exist to tabulate city population. For example:
+
+- <https://simplemaps.com/data/gb-cities>
+- <https://en.wikipedia.org/wiki/Birmingham>
+- <https://worldpopulationreview.com/world-cities/birmingham-population>
+- <https://www.ons.gov.uk/visualisations/censuspopulationchange/E08000025/>
+- <https://populationdata.org.uk/population-of-birmingham/>
+- <https://www.citypopulation.de/en/uk/admin/west_midlands/E08000025__birmingham/>
---
+++ ring-populations/data/uk_rings/scrape.py
@@ -0,0 +1,79 @@
+"""Get the population around a point for some UK cities
+"""
+
+import json
+import csv
+import time
+import requests
+from tqdm import tqdm
+
+CITIES_CSV = "./data/worldcities/worldcities.csv"
+OUTPUT_JSON = "./data/uk_rings/uk_rings.json"
+
+SCRAPE_TOP_BY_POPULATION = 20
+MAX_DISTANCE_KM = 20
+
+RING_URL = "https://ringpopulationsapi.azurewebsites.net/api/globalringpopulations"
+
+
+def get_rings(city):
+    url = f"{RING_URL}?latitude={city['lat']}&longitude={city['lng']}&distance_km={MAX_DISTANCE_KM}"
+    print(f"requesting\n{url}")
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    raise LookupError(
+        f"Error getting rings for {city['city_ascii']}: {response.status_code}"
+    )
+
+
+def main():
+    """Scrape data from population around a point for some UK cities"""
+
+    # all cities
+    all_cities = []
+    with open(CITIES_CSV, "r", encoding="utf-8") as cities_file:
+        cities_reader = csv.DictReader(cities_file)
+        for city in cities_reader:
+            all_cities.append(city)
+
+    # uk cities
+    uk_cities = [city for city in all_cities if city["country"] == "United Kingdom"]
+    # by population
+    uk_cities.sort(key=lambda city: int(city["population"]), reverse=True)
+    # top N
+    uk_cities = uk_cities[:SCRAPE_TOP_BY_POPULATION]
+
+    # existing data
+    with open(OUTPUT_JSON, "r", encoding="utf-8") as output_file:
+        existing_data = json.load(output_file)
+
+    # scrape
+    for city in tqdm(uk_cities, total=len(uk_cities)):
+        if (name := city["city_ascii"]) not in existing_data:
+            print(f"Scraping {name}")
+            data = get_rings(city)
+            # transform
+            distances = [ring["distance"] for ring in data]
+            populations = [ring["people"] for ring in data]
+            bus_stops = [ring["busStops"] for ring in data]
+            tram_stops = [ring["tramStops"] for ring in data]
+            rail_stops = [ring["railStops"] for ring in data]
+            # write
+            existing_data[name] = {
+                "distances": distances,
+                "populations": populations,
+                "busStops": bus_stops,
+                "tramStops": tram_stops,
+                "railStops": rail_stops,
+            }
+            with open(OUTPUT_JSON, "w", encoding="utf-8") as output_file:
+                json.dump(existing_data, output_file, separators=(",", ":"))
+            # wait
+            time.sleep(1)
+
+    print("Done")
+
+
+if __name__ == "__main__":
+    main()
