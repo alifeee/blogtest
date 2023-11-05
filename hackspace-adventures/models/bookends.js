ae804bc62818fb055d9ec7acc06a53d0a571e671
alifeee <alifeeerenn@hotmail.com>
2023-11-05 01:26:47 +0000

    add missing shapes

--- hackspace-adventures/models/bookends.js
+++ hackspace-adventures/models/bookends.js
@@ -29,31 +29,107 @@ class BookendModel extends HTMLElement {
 
     this.illo = new Zdog.Illustration({
       element: canvas,
-      zoom: 4,
+      zoom: 1,
       dragRotate: true,
       onDragStart: () => this.setRotating(false),
       onDragEnd: () => this.setRotating(true),
+      rotate: { y: -Zdog.TAU / 16, x: -Zdog.TAU / 32 },
+      translate: { y: -22 / 2, z: -89 / 2 },
     });
 
-    new Zdog.Ellipse({
+    let front = new Zdog.Box({
       addTo: this.illo,
-      diameter: 20,
-      translate: { z: 10 },
-      stroke: 5,
-      color: "#636",
+      width: 86,
+      height: 99,
+      depth: 22,
+      stroke: false,
+      color: "#d9b27e",
+      translate: { z: 89 / 2 },
     });
 
-    new Zdog.Rect({
-      addTo: this.illo,
-      width: 20,
-      height: 20,
-      translate: { z: -10 },
-      stroke: 3,
-      color: "#E62",
+    let bottom = new Zdog.Box({
+      addTo: front,
+      width: 86,
+      height: 89,
+      depth: 22,
+      stroke: false,
+      color: "#debb8c",
+      rotate: { x: Zdog.TAU / 4 },
+      translate: { y: 99 / 2 + 22 / 2, z: -89 / 2 + 22 / 2 },
+    });
+
+    let slantBack = new Zdog.Rect({
+      addTo: front,
+      width: 86,
+      height: 91,
       fill: true,
+      color: "#ebc18a",
+      rotate: { x: -Math.atan(67 / 64) },
+      translate: { y: 35 / 2, z: -89 / 2 },
     });
 
-    // start animation
+    let slantFront = new Zdog.Rect({
+      addTo: front,
+      width: 86,
+      height: 48,
+      fill: true,
+      color: "#ebc18a",
+      rotate: { x: -Math.atan(31 / 33) },
+      translate: { y: 66 / 2, z: -53 / 2 },
+    });
+
+    let slantSideAnchor = new Zdog.Anchor({
+      addTo: front,
+      translate: { x: 86 / 2, y: 99 / 2, z: 22 / 2 },
+    });
+
+    let slantRightSide = new Zdog.Shape({
+      addTo: slantSideAnchor,
+      path: [
+        { z: -22, y: -64 },
+        { z: -89, y: 0 },
+        { z: -53, y: 0 },
+        { z: -22, y: -33 },
+      ],
+      closed: true,
+      fill: true,
+      color: "#ebc18a",
+    });
+
+    let slantLeftSide = slantRightSide.copy({
+      translate: { x: -86 },
+    });
+
+    let slantScrewTop = new Zdog.Ellipse({
+      addTo: slantBack,
+      diameter: 6,
+      stroke: false,
+      fill: true,
+      color: "#b5b5b5",
+      translate: { z: 0, y: -25 },
+      stroke: 3,
+    });
+
+    let slantScrewBottom = slantScrewTop.copy({
+      translate: { z: 0, y: 25 },
+    });
+
+    let bottomScrewRight = new Zdog.Ellipse({
+      addTo: front,
+      diameter: 6,
+      stroke: false,
+      fill: true,
+      color: "#b5b5b5",
+      rotate: { x: Zdog.TAU / 4 },
+      translate: { y: 99 / 2 + 22, x: 86 / 2 - 15 },
+      stroke: 3,
+    });
+
+    let bottomScrewLeft = bottomScrewRight.copy({
+      translate: { y: 99 / 2 + 22, x: -86 / 2 + 15 },
+    });
+
+    this.illo.updateRenderGraph();
     this.animate();
   }
 
