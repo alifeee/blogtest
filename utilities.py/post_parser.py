93dbe866a3c51f6360cc9fd2404ff902c29df826
alifeee <alifeeerenn@hotmail.com>
2023-11-11 02:09:23 +0000

    add html and md_url to post_parser

--- utilities.py/post_parser.py
+++ utilities.py/post_parser.py
@@ -3,7 +3,9 @@
 
 from dataclasses import dataclass
 from datetime import datetime
+import os
 from typing import List
+import pprint
 
 # pip install tzdata, beautifulsoup4
 from zoneinfo import ZoneInfo  # timezone
@@ -16,9 +18,11 @@ INDEX_FILE = "index.html"
 class Post:
     """A dataclass to hold a blog post"""
 
+    html: str
     title: str
     description: str
     relative_url: str
+    md_url: str
     date: datetime
 
 
@@ -29,7 +33,13 @@ def get_all_posts() -> List[Post]:
 
     posts = []
     for post in posts_html:
+        html = str(post)
         relative_url = post.find("a", class_="link")["href"]
+        # md url is f"{relative_url}/{relative_url}.md"
+        #  with "./\" stripped from relative_url
+        title_filename = relative_url.lstrip("./")
+        md_url = f"{relative_url}/{title_filename}.md"
+
         date_str = post.find("time")["datetime"]
         date = datetime.strptime(date_str, "%Y-%m-%d")
         date = date.replace(tzinfo=ZoneInfo("Etc/UTC"))
@@ -42,12 +52,23 @@ def get_all_posts() -> List[Post]:
 
         posts.append(
             Post(
+                html=html,
                 title=title,
                 description=description,
                 relative_url=relative_url,
+                md_url=md_url,
                 date=date,
             )
         )
+
+    # make sure all relative_url's exist
+    for post in posts:
+        assert os.path.exists(
+            post.relative_url
+        ), f"post {post.relative_url} does not exist"
+    # make sure all md_url's exist
+    for post in posts:
+        assert os.path.exists(post.md_url), f"post {post.md_url} does not exist"
 
     posts.sort(key=lambda x: x.date, reverse=True)
     return posts
@@ -58,4 +79,5 @@ if __name__ == "__main__":
     ps = get_all_posts()
     print("posts:")
     for p in ps:
-        print(p)
+        pprint.pprint(p)
+        print()
